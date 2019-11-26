f103a269fb5c1f3be5b9eeb309f4d73e743585f5
corbinSanders <corbin@cos.io>
2019-11-26 16:02:59 -0500

    Making script changes

--- settings/defaults.py
+++ settings/defaults.py
@@ -2,8 +2,8 @@
 
 DOI_FORMAT = '10.70102/fk2osf.io/{guid}'
 
-DATACITE_USERNAME = 'DEMO.COS'
-DATACITE_PASSWORD = 'C0SCO$Cos'
+DATACITE_USERNAME = ''
+DATACITE_PASSWORD = ''
 DATACITE_URL = 'https://doi.test.datacite.org/'
 DATACITE_PREFIX = '10.70102'  # Datacite's test DOI prefix -- update in production
 
@@ -11,10 +11,10 @@ CHUNK_SIZE = 1000
 PAGE_SIZE = 100
 OSF_API_URI = 'https://api.staging.osf.io/'
 OSF_COLLECTION_NAME = 'cos-dev-sandbox'
-OSF_BEARER_TOKEN = 'Q8ujF8KiShyLimNEtusSWsgvkQGYTQTUQ2XOZ1POTU2DmHLquHNc7cZT0MYzbHJUY1wMTE'
+OSF_BEARER_TOKEN = ''
 
-IA_ACCESS_KEY = 'SyajvXxB4MhUtQDD'
-IA_SECRET_KEY = 'ejtMwMWwwyB2HtH8'
+IA_ACCESS_KEY = ''
+IA_SECRET_KEY = ''
 
 OSF_API_URL = 'https://api.staging.osf.io/'
 OSF_FILES_URL = 'https://files.us.staging.osf.io/'
